e92713f43c4c062af35a2837ef7c61e562656a8a
Konstantin <konstantin.kif@mail.ru>
2022-09-09 01:27:25 +0700

    add task2

--- HW9/task1/Program.cs
+++ HW9/task1/Program.cs
@@ -1,5 +1,5 @@
-﻿ // Задайте значения M и N. Напишите программу, которая выведет 
- // все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
@@ -9,16 +9,20 @@ int Prompt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void ShowNaturalRow(int min, int max)
+void EvenNaturalNumbersRow(int min, int max)
 {
-    if (min > max)
+    int i = 0;
+    for (i = min; i > max; i++)
     {
-        return;
+        if (i % 2 == 0)
+        {
+           return;
+        }
     }
-    ShowNaturalRow(min, max - 1);
-    System.Console.WriteLine(max);
+    EvenNaturalNumbersRow(min, max - 1);
+    Console.WriteLine(max);
 }
 
-int min = Prompt ("Enter a number M -> ");
-int max = Prompt ("Enter a number n -> ");
-ShowNaturalRow(min, max);
+int min = Prompt("Enter a number M -> ");
+int max = Prompt("Enter a number N -> ");
+EvenNaturalNumbersRow(min, max);
---
+++ HW9/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//   Напишите программу,
+// которая найдёт сумму числа с помощью рекурсии.
+// 345 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfNaturalElements(int number)
+{
+    
+  if  (number == 0)
+    {
+        return 0;
+    }
+    return SumOfNaturalElements(number / 10) + number % 10;
+}
+
+int number = Prompt ("Enter your number -> ");
+int sum = SumOfNaturalElements(number);
+Console.WriteLine(sum);
